e4b87a21a2a66799ca42445dc4da7a2f75aa0d48
Robert Lynch <relynch10@gmail.com>
2023-04-14 15:45:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(fun = "roller-skate"){
+   return `This Saturday, I want to ${fun}!`
+}
+saturdayFun()
+
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+mondayWork()
+
+function wrapAdjective(adj = "*"){
+    return function (secretWord = "special"){
+        return `You are ${adj}${secretWord}${adj}!`
+    }
+}
+wrapAdjective()
